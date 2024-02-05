16b6a1189e7d4ed274104c69414e2402a5ee93b3
Jay <jay3105@hotmail.com>
2024-02-05 14:48:56 +0100

    Added TreeView

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import {Component} from '@angular/core';
 import {MatIconRegistry} from "@angular/material/icon";
 import {DomSanitizer} from "@angular/platform-browser";
-import {DatabaseService, User} from "./services/database.service";
-import {SplashScreen} from "@capacitor/splash-screen";
 
 
 
@@ -22,12 +20,8 @@ export class AppComponent {
 
   private iconPath: string = "../assets/icons/";
 
-  users = this.database.getUsers();
-  newUserName = '';
-
   constructor(private matIconRegistry: MatIconRegistry,
-              private domSanitizer: DomSanitizer,
-              private database: DatabaseService) {
+              private domSanitizer: DomSanitizer) {
 
     this.icons
       .map(iconName => {return {name: iconName, path: this.iconPath + iconName + ".svg"}})
@@ -36,37 +30,6 @@ export class AppComponent {
           icon.name,
           this.domSanitizer.bypassSecurityTrustResourceUrl(icon.path))
       })
-    this.initApp();
-  }
-
-  async createUser() {
-    await this.database.addUser(this.newUserName);
-    this.newUserName = '';
-  }
-
-  updateUser(user: User) {
-    const active = user.active ? 1 : 0;
-    this.database.updateUserById(user.id, active);
-  }
-
-  deleteUser(user: User) {
-    this.database.deleteUserById(user.id);
-  }
-
-  async initApp() {
-    await this.database.initializePlugin();
-    SplashScreen.hide()
-    this.database.addUser('Max');
-    this.database.addUser('Moritz');
-    this.database.addUser('Hans');
-  }
-
-  save() {
-    alert("Save");
-  }
-
-  load() {
-    alert("Load");
   }
 
 }
